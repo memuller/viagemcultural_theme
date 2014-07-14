eaa858f7a33b78984598387cc66ed02957eccaf9
Thiago Zucareli <thiagozp@gmail.com>
2014-07-14 14:30:19 -0300

    Defining thumbnails size

--- functions.php
+++ functions.php
@@ -1,3 +1,6 @@
 <?php
 require_once 'lib/enqueue.php';
+
+add_image_size( 'trip_thumbnail', 940, 410, array( 'center', 'center' ));
+add_image_size( 'tip_thumbnail', 300, 250, array( 'center', 'center' ));
 ?>
