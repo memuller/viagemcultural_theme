9df0b5da397f79ac8cbf5394f08c999f0e6ab77d
Matheus E. Muller <hello@memuller.com>
2014-07-17 19:33:43 -0300

    Adds thumbnail support.

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'lib/enqueue.php';
-
+add_theme_support( 'post-thumbnails' ); 
 add_image_size( 'trip_thumbnail', 940, 410, array( 'center', 'center' ));
 add_image_size( 'tip_thumbnail', 300, 250, array( 'center', 'center' ));
 
