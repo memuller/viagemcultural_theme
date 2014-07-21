2887514b1249872e33c8475bdc813858f4bb59cc
Thiago Zucareli <thiagozp@gmail.com>
2014-07-21 08:51:45 -0300

    style

--- lib/enqueue.php
+++ lib/enqueue.php
@@ -4,7 +4,7 @@
  */
 
 add_action('wp_enqueue_scripts', function () {
-  wp_enqueue_style('main_css', get_stylesheet_directory_uri() . '/assets/main.min.css', false, 'a6921dbe663370751855f13acb74216c');
+  wp_enqueue_style('main_css', get_stylesheet_directory_uri() . '/assets/main.min.css', false, 'dd083ccdd40017e3debe45d17b202854');
 
   wp_register_script('main_scripts', get_stylesheet_directory_uri() . '/assets/main.min.js', array('jquery'), '68b329da9893e34099c7d8ad5cb9c940', false);
   wp_enqueue_script( 'main_scripts');
