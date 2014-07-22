2be5ebe319cf00774a89cb5f49b4fd777e835fe9
Thiago Zucareli <thiagozp@gmail.com>
2014-07-22 20:03:30 -0300

    Sidebar's new style;Title height issue;
    Adding sidebar new style to video link according w/ video archive &
    resolving title height bug.

--- sidebar.php
+++ sidebar.php
@@ -11,7 +11,10 @@
                     <?php foreach ($post_object->children('video') as $video): ?>
                         <li class="grid_4 alpha omega">
                             <a href="<?php echo $video->permalink ?>">
-                                <?php echo get_the_post_thumbnail($video->base->ID, 'sidebar_thumbnail', array('class' => 'grid_2 alpha')) ?>
+                                <div class="episode-image clearfix">
+                                    <?php echo get_the_post_thumbnail($video->base->ID, 'sidebar_thumbnail', array('class' => 'grid_2 alpha')) ?>
+                                    <img class="play" src="<?php bloginfo('template_directory'); ?>/images/play.png"/>
+                                </div>
                                 <h2 class="grid_2 omega"><?php echo $video->title ?></h2>
                                 <span class="exhibition grid_2 omega">Programa do dia: <time><?php echo $video->broadcast_date ?></time></span>
                             </a>
