4e9c523d9cf24a5610ee3dd853bce0e47107c36e
Thiago Zucareli <thiagozp@gmail.com>
2014-07-22 18:01:36 -0300

    videos archive layout improvement;

--- archive-video.php
+++ archive-video.php
@@ -9,19 +9,22 @@ get_header(); ?>
         <h1 class="grid_8">Episódios do programa</h1>
     </div>
     <div class="box container_12">
-        <ul class="episodes">
+        <ul class="episodes clearfix">
             <?php while (have_posts()) : the_post(); $video = new \ViagemCultural\Video($post); $travel = $video->parent();  ?>
-                <li class="episode shadow grid_6 alpha omega">
+                <li class="episode grid_11 alpha omega">
                     <div class="episode-image">
-                        <?php echo get_the_post_thumbnail($post->ID, 'episode_thumbnail') ?>
+                        <?php echo get_the_post_thumbnail($post->ID, 'episode_thumbnail', array('class' => 'scale', 'data-scale' => 'best-fill', 'data-align' => 'center')) ?>
+                        <img class="play" src="<?php bloginfo('template_directory'); ?>/images/play.png"/>
                     </div>
                     <div class="episode-info">
                         <h2><?php the_title() ?></h2>
-                        <?php if ($travel): ?>
-                            <a href="<?php echo $travel->permalink ?>" class="button-blue">Ver dica</a>    
-                        <?php endif ?>
-                        
-                        <a href="<?php echo $video->permalink ?>" class="button-blue">Assistir o programa</a>
+                        <div class="buttons">
+                            <?php if ($travel): ?>
+                                <a href="<?php echo $travel->permalink ?>" class="button-blue">Ver dica</a>    
+                            <?php endif ?>
+                            <a href="<?php echo $video->permalink ?>" class="button-blue">Assistir o programa</a>
+                            <span>Programa exibido dia: <time><?php the_('broadcast_date') ?></time></span>
+                        </div>
                     </div>
                 </li>
             <?php endwhile; ?>
--- header.php
+++ header.php
@@ -16,15 +16,6 @@
         <nav class="redetv-bar">
             <div class="container_12">
                 <a class="redetv-brand grid_2 alpha" href="http://www.redetv.uol.com.br/"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/redetv.png"></a>
-                <ul class="redetv-menu grid_10">
-                    <li><a href="http://www.redetv.uol.com.br/grade.aspx">Programação</a></li>
-                    <li><a href="http://www.redetv.uol.com.br/tvfama/">TV Fama</a></li>
-                    <li><a href="http://www.redetv.uol.com.br/jornalismo/">Notícias</a></li>
-                    <li><a href="http://www.redetv.uol.com.br/esportes/">Esporte</a></li>
-                    <li><a href="http://www.redetv.uol.com.br/videos/">Vídeos</a></li>
-                    <li><a href="http://www.redetv.uol.com.br/ezueira/">É Zueira</a></li>
-                    <li><a href="http://www.redetv.uol.com.br/receitatododia/">Receita todo dia</a></li>
-                </ul>
             </div>
         </nav>
         <section class="header-content">
