95132affd7d9ea52cda9e428a3c147026d109fbf
Thiago Zucareli <thiagozp@gmail.com>
2014-07-23 15:31:14 -0300

    Contact page

--- contact.php
+++ contact.php
@@ -1,10 +1,14 @@
-<?php get_header(); ?>
+<?php
+/*
+Template Name: Contato
+*/
+get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
 <section class="container">
     <div class="container_12">
         <h1 class="grid_8"><?php the_title(); ?></h1>
     </div>
-    <div class="box entry-content container_12 clearfix">
+    <div class="box contact-entry container_12 clearfix">
         <?php the_content(); ?>
     </div>
 </section>
