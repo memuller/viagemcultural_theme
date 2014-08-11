f37591bdad207780f9c06410e705bba4a8525a06
Matheus E. Muller <hello@memuller.com>
2014-08-11 17:11:16 -0300

    Localizes "Wordpress" JSvars.

--- lib/enqueue.php
+++ lib/enqueue.php
@@ -8,6 +8,10 @@ add_action('wp_enqueue_scripts', function () {
 
   wp_register_script('main_scripts', get_stylesheet_directory_uri() . '/assets/main.min.js', array('jquery'), 'c320834193c0fe3fab60174f32edef70', true);
   wp_enqueue_script( 'main_scripts');
+  wp_localize_script('main_scripts', 'Wordpress', array(
+    'ajax_url' => admin_url('admin-ajax.php'),
+    'home_url' => home_url()
+  ));
 });
 if (defined('GOOGLE_ANALYTICS_ID')) {
   add_action('wp_footer', function(){ ?>
