ce741ab9596573823de72857e442e0e353b53308
Matheus E. Muller <hello@memuller.com>
2014-08-14 14:09:35 -0300

    Adds a placeholder for when no content is available on video archives.

--- archive-video.php
+++ archive-video.php
@@ -9,32 +9,36 @@ get_header(); global $posts; ?>
         <h1 class="grid_8">Episódios do programa</h1>
     </div>
     <div class="box container_12">
-        <ul class="episodes clearfix">
-            <?php foreach ($posts as $post) { setup_postdata($post); $video = new \ViagemCultural\Video($post); $travel = $video->parent();  ?>
-                <li class="episode grid_11 alpha omega">
-                    <div class="episode-image">
-                        <?php echo get_the_post_thumbnail($post->ID, 'episode_thumbnail', array('class' => 'scale', 'data-scale' => 'best-fill', 'data-align' => 'center')) ?>
-                        <img class="play" src="<?php bloginfo('template_directory'); ?>/images/play.png"/>
-                    </div>
-                    <div class="episode-info">
-                        <h2><?php the_title() ?></h2>
-                        <div class="buttons">
-                            <?php if ($travel): ?>
-                                <a href="<?php echo $travel->permalink ?>" class="button-blue">Ver dica</a>    
-                            <?php endif ?>
-                            <a href="<?php echo $video->permalink ?>" class="button-blue">Assistir o programa</a>
-                            <span>Programa exibido dia: <time><?php echo get_the_('broadcast_date') ?></time></span>
+        <?php if (empty($posts)): ?>
+            <?php //Coisa para ser exibida quando nenhum resultado estiver presente ?>        
+        <?php else: ?>
+            <ul class="episodes clearfix">
+                <?php foreach ($posts as $post) { setup_postdata($post); $video = new \ViagemCultural\Video($post); $travel = $video->parent();  ?>
+                    <li class="episode grid_11 alpha omega">
+                        <div class="episode-image">
+                            <?php echo get_the_post_thumbnail($post->ID, 'episode_thumbnail', array('class' => 'scale', 'data-scale' => 'best-fill', 'data-align' => 'center')) ?>
+                            <img class="play" src="<?php bloginfo('template_directory'); ?>/images/play.png"/>
                         </div>
-                    </div>
-                </li>
-            <?php } ?>
-        </ul>
-        <div class="pagination">
-            <div class="pagination-buttons">
-                <?php next_posts_link('Episódios mais novos') ?>
-                <?php previous_posts_link('Episódios mais antigos') ?>
+                        <div class="episode-info">
+                            <h2><?php the_title() ?></h2>
+                            <div class="buttons">
+                                <?php if ($travel): ?>
+                                    <a href="<?php echo $travel->permalink ?>" class="button-blue">Ver dica</a>    
+                                <?php endif ?>
+                                <a href="<?php echo $video->permalink ?>" class="button-blue">Assistir o programa</a>
+                                <span>Programa exibido dia: <time><?php echo get_the_('broadcast_date') ?></time></span>
+                            </div>
+                        </div>
+                    </li>
+                <?php } ?>
+            </ul>
+            <div class="pagination">
+                <div class="pagination-buttons">
+                    <?php next_posts_link('Episódios mais novos') ?>
+                    <?php previous_posts_link('Episódios mais antigos') ?>
+                </div>
             </div>
-        </div>
+        <?php endif ?>
     </div>
 </section>
 
